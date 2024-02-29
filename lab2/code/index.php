ab8df0fa8f5b586fe9e32932dd574f971c55801e
HassuK <fronakactiv00@mail.ru>
2024-02-29 22:33:02 +0200

    added task 2: multiplication and division. Also added screens of output

--- lab2/code/index.php
+++ lab2/code/index.php
@@ -1,6 +1,7 @@
 <?php
 
-$a=123;
+//task 2 numbers
+/* $a=123;
 echo $a;
 echo "\n";
 
@@ -16,4 +17,12 @@ echo "\n";
 $last_month=1187.23;
 $this_month=1089.98;
 echo $last_month-$this_month;
+echo "\n"; */
+
+//task 11, multiplication and division
+$num_languages=4;
+$months=11;
+$days=$months*16;
+$days_per_language=$days/$num_languages;
+echo $days_per_language;
 echo "\n";
