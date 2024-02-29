518535593c23863e9a109e2be3fddbf2c3c3e706
HassuK <fronakactiv00@mail.ru>
2024-02-29 22:25:51 +0200

    task 2: numbers

--- lab2/code/index.php
+++ lab2/code/index.php
@@ -1,2 +1,19 @@
 <?php
-echo phpinfo();
+
+$a=123;
+echo $a;
+echo "\n";
+
+$float =1.3;
+echo $float;
+echo "\n";
+
+$b=111;
+echo $a-$b;
+echo "\n";
+
+
+$last_month=1187.23;
+$this_month=1089.98;
+echo $last_month-$this_month;
+echo "\n";
